c122d6d8a650b5c2619352c2eec55572b96325aa
Ayeehyemi <aishaabdullmumin@gmail.com>
2025-02-06 11:26:08 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var typed= new Typed(".bab",{
+    strings:["Frontend developer" , "Web Developer"],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 1000,
+loop: true
+});
+
+
+
+var typed= new Typed(".dada",{
+    strings:["Abdulmumin" ],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 1000,
+loop: true
+});
